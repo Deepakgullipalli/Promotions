731ca400754f50103d8f35afcd72cb03975a66e9
Deepak Gullipalli <deepakgullipalli@yahoo.com>
2021-05-16 21:24:13 +0530

    Add project files.

---
+++ Promotions.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Promotions.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ Promotions.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Promotions.Core
+{
+    public class Class1
+    {
+    }
+}
